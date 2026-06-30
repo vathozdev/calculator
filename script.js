e370658270878a606e22f0e598ce15ffa7823253
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-30 20:23:58 +0300

    Define operations for basic arithmetic functions

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ firstNumber = null;
 operator = null;
 justComputed = true;
 errorOccured = false;
+
+operations = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b
+};
